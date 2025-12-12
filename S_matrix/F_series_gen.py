cff7fd9af449ced8eb91f21f712f53cc5a058ca1
Serana <568324604@qq.com>
2025-12-12 16:32:11 +0800

    2025.12.12 16:32

--- S_matrix/F_series_gen.py
+++ S_matrix/F_series_gen.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def F_series_gen(array,nDim,cut_small=True):
-    fourier_coeffi=np.fft.fftshift(np.fft.fft(array))
+    fourier_coeffi=np.fft.fftshift(np.fft.fft(array)/array.shape[0])
+    # fourier_coeffi=np.fft.fft(array)
     tol=1e-9
     if cut_small==True:
         ind_small_real=(np.abs(np.real(fourier_coeffi))<tol)
