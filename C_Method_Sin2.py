a703067637800d31308d95837e5a1b488d48d744
Serana <568324604@qq.com>
2026-04-27 09:52:56 +0800

    2026.4.27 9:52

---
+++ C_Method_Sin2.py
@@ -0,0 +1,314 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from S_matrix.Grating import Triangular,Sinusoidal,Blazed
+from C_Method.SetConstantByPolar import setConstanByPola
+from S_matrix.F_series_gen import F_series_gen
+from C_Method.Toeplitze import Toeplitz
+from C_Method.Eigen import Eigen
+from C_Method.Plot_intensity import Plot_intensity
+from C_Method.Plot_Effi import Plot_Effi
+from scipy.special import jv
+
+'''
+对于正弦函数的特殊情况,特意重写为此文件
+'''
+
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False#解决plt画图中文乱码问题
+
+def Bessel_fourier_coeffi_sin(beta,depth,k0,m1,m2):
+    '''
+    计算exp(j*beta*a(x))的Fourier系数
+    a(x)=depth/2*(1+sin(Kx))
+    '''
+    m=np.arange(m1,m2+1)
+    z=beta*k0*depth/2
+    coeffi=np.exp(1j*z)*jv(m,z)
+    return coeffi
+    
+def Bessel_fourier_coeffi_cos(beta,k0,m1,m2,Constant):
+    '''
+    计算exp(j*beta*a'(x))的fourier系数
+    a'(x)=depth*K/2*cos(Kx)
+    '''
+    m=np.arange(m1,m2+1)
+    z=beta*Constant['depth']*np.pi*k0/Constant['period']
+    coeffi=(1j)**m*jv(m,z)
+    return coeffi
+
+def GenerateFFieldsChand(a,b0,nDim,k0,d,m1,m2,real_Ray2_idx,real_Ray1_idx,SB1,SB2,Constant):
+    # LP_fun=np.exp(-1j*b0*a)#define fourier transform argument for positive fields
+    # F_in_0=F_series_gen(LP_fun,nDim)#正场系数
+    # q0=len(F_in_0)//2
+    # F_in=F_in_0[q0-(nDim-1)//2:q0+(nDim-1)//2+1]
+    Fm0=Bessel_fourier_coeffi_sin(b0,Constant['depth'],k0,m1,m2)
+    Fmk=np.zeros((nDim,len(real_Ray2_idx)),dtype=np.complex128)#F_mk_R_N
+    Fmn=np.zeros((nDim,len(real_Ray1_idx)),dtype=np.complex128)#F_mn_R_P
+    if len(real_Ray1_idx)!=0:
+        min_ray1=np.min(real_Ray1_idx)
+        max_ray1=np.max(real_Ray1_idx)
+        for M in range(min_ray1,max_ray1+1):
+            # LPn_fun=np.exp(-1j*SB1[M-m1]*a)
+            # FRP0=F_series_gen(LPn_fun,nDim)
+            beta=SB1[M-m1]
+            Fmn0=Bessel_fourier_coeffi_sin(beta,Constant['depth'],k0,-nDim,nDim)
+            # FRP[:,M-min_ray1]=Bessel_fourier_coeffi(beta,Constant['depth'],m1,m2)
+            q0=len(Fmn0)//2
+            part1=np.conj(Fmn0[q0+m1-M:q0])
+            part2=np.conj(Fmn0[q0:q0+m2-M+1])
+            Fmn[:,M-min_ray1]=np.concatenate([part1,part2])
+
+    if len(real_Ray2_idx)!=0:
+        min_ray2=np.min(real_Ray2_idx)
+        max_ray2=np.max(real_Ray2_idx)
+        for k in range(min_ray2,max_ray2+1):
+            # LNn_fun=np.exp(-1j*SB2[k]*a)
+            # FRN0=F_series_gen(LNn_fun,nDim)
+            beta=SB2[k-m1]
+            Fmk0=Bessel_fourier_coeffi_sin(beta,Constant['depth'],k0,-nDim,nDim)
+            q0=len(Fmk0)//2
+            for m in range(m1,m2+1):
+                Fmk[m-m1,k-min_ray2]=Fmk0[q0+m-k]
+    return Fm0,Fmk,Fmn
+
+def GenerateGFieldsChand(b0,a_mat,SB1,SB2,real_Ray1_idx,real_Ray2_idx,\
+    m1,m2,nDim,imag_eig1p,imag_eig2n,Fmn,Fmk,Fm0,Fmq,Fmr,A,eps1,eps2,Constant):
+    Gmn=np.zeros((nDim,len(real_Ray1_idx)),dtype=complex)
+    Gmk=np.zeros((nDim,len(real_Ray2_idx)),dtype=complex)
+    Gm0=np.zeros((nDim,1),dtype=complex)
+    Gmq=np.zeros((nDim,len(imag_eig1p)),dtype=complex)
+    Gmr=np.zeros((nDim,len(imag_eig2n)),dtype=complex)
+    AUX_mat=np.eye(nDim)+a_mat@a_mat
+    #计算Gmn(实特征值正场)
+    if len(real_Ray1_idx)!=0:
+        min_ray1=np.min(real_Ray1_idx)
+        for m in range(nDim):
+            for n in range(len(real_Ray1_idx)):
+                sb1_idx=n+min_ray1-m1
+                beta_n=SB1[sb1_idx]
+                for s in range(nDim):
+                    term=a_mat[m,s]*A[s]-AUX_mat[m,s]*beta_n
+                    Gmn[m,n]+=term*Fmn[s,n]
+    #计算Gmk(实特征值负场)
+    if len(real_Ray2_idx)!=0:
+        min_ray2=np.min(real_Ray2_idx)
+        for m in range(nDim):
+            for k in range(len(real_Ray2_idx)):
+                sb2_idx=k+min_ray2-m1
+                beta_k=SB2[sb2_idx]
+                for s in range(nDim):
+                    term=a_mat[m,s]*A[s]+AUX_mat[m,s]*beta_k
+                    Gmk[m,k]+=term*Fmk[s,k]
+    #计算Gm0
+    for m in range(nDim):
+        for s in range(nDim):
+            term=a_mat[m,s]*A[s]+AUX_mat[m,s]*b0
+            Gm0[m,0]+=term*Fm0[s]
+    #计算Gmq,Gmr(虚特征值场)
+    for m in range(nDim):
+        for q in range(len(imag_eig1p)):
+            rho_q=imag_eig1p[q]
+            for s in range(nDim):
+                term=a_mat[m,s]*A[s]-AUX_mat[m,s]*rho_q
+                Gmq[m,q]+=term*Fmq[s,q]
+    for m in range(nDim):
+        for r in range(len(imag_eig2n)):
+            rho_r=imag_eig2n[r]
+            for s in range(nDim):
+                term=a_mat[m,s]*A[s]-AUX_mat[m,s]*rho_r
+                Gmr[m,r]+=term*Fmr[s,r]
+    #归一化处理
+    Gmn=(1/eps1)*Gmn
+    Gmk=(1/eps2)*Gmk
+    Gm0=(1/eps1)*Gm0
+    Gmq=(1/eps1)*Gmq
+    Gmr=(1/eps2)*Gmr
+    return Gmn,Gmk,Gm0,Gmq,Gmr
+
+def Compute(n1,n2,polar,Constant):
+    Constant=setConstanByPola(n1,n2,polar,Constant)
+    k0=2*np.pi/Constant['wavelength']#波数
+    K=2*np.pi/Constant['period']#倒易空间矢量
+    nDim=Constant['n_Tr']#计算所用的总模数
+    alpha0=Constant['n1']*k0*np.sin(Constant['thetai'])
+    m1=int(-(nDim-1)/2)
+    m2=int((nDim-1)/2)
+    nDim=m2-m1+1
+    A=(alpha0*np.ones((1,m2-m1+1))+K*np.linspace(m1,m2,nDim))/k0#文献中的alpham
+    A=A.flatten()
+    B1=Constant['n1']**2-A*A#beta1**2
+    B1=B1.flatten()
+    B2=Constant['n2']**2-A*A#beta2**2
+    B2=B2.flatten()
+    SB1=np.sqrt(B1,dtype=complex)#eigenvalues of incident medium
+    SB1_idx=(abs(np.imag(SB1))==0)&(np.real(SB1)>0)#indices of real propogation orders, in incident medium
+    SB1_ind=np.arange(m1,m2+1)#indices of all modes
+    real_Ray1_idx=SB1_ind[SB1_idx]#indices of real propogation numbers
+    Constant['real_Ray1_idx']=real_Ray1_idx
+    SB2=-np.sqrt(B2,dtype=complex)#eigenvalues of transmition medium
+    SB2_idx2=(np.abs(np.imag(SB2))==0)&(np.real(SB2)<0)#indices of real propogation order
+    SB2_ind2=np.arange(m1,m2+1)
+    real_Ray2_idx=SB2_ind2[SB2_idx2]
+    Constant['real_Ray2_idx']=real_Ray2_idx
+    ###对a_diff_vec采用Bessel展开
+    a_diff_vec=F_series_gen(a_diff,nDim)
+    # a_diff_vec=Bessel_fourier_coeffi_cos(1,k0,-nDim,nDim,Constant)
+    ##观察傅里叶频谱分量
+    # temp_x=range(-nDim,nDim+1)
+    # plt.plot(temp_x,a_diff_vec)
+    # plt.show()
+    #####
+    a_mat=Toeplitz(a_diff_vec,nDim)
+    V1,rho1,V2,rho2=Eigen(A,B1,B2,a_mat,nDim)
+    # real_eig1p,real_eig2n,imag_eig1p,imag_eig2n,imag_Vec1p,imag_Vec2n=Sort(V1,rho1,V2,rho2,Constant['accuracy'],nDim)
+    real_eig1p,real_eig2n,imag_eig1p,imag_eig2n,imag_Vec1p,imag_Vec2n=SortEigenvalueChand(V1,rho1,V2,rho2,Constant['accuracy'],Constant)
+    Fmq=imag_Vec1p
+    Fmr=imag_Vec2n
+    #Assemble F-matrices
+    b0=np.sqrt(B1[-m1])
+    Fm0,Fmk,Fmn=GenerateFFieldsChand(Constant['a'],b0,nDim,k0,Constant['period'],m1,m2,real_Ray2_idx,real_Ray1_idx,SB1,SB2,Constant)
+    #Assemble G-matrices
+    Gmn,Gmk,Gm0,Gmq,Gmr=GenerateGFieldsChand(b0,a_mat,SB1,SB2,real_Ray1_idx,real_Ray2_idx,m1,m2,nDim,imag_eig1p,
+    imag_eig2n,Fmn,Fmk,Fm0,Fmq,Fmr,A,Constant['eps1'],Constant['eps2'],Constant)
+    #Assemble matrix for matching boundary conditions and solve the linear system
+    Gm0=Gm0.T
+    Gm0=Gm0.flatten()
+    MatBC=np.block([[Fmn,Fmq,-Fmk,-Fmr],[Gmn,Gmq,-Gmk,-Gmr]])
+    VecBC=np.concatenate((-Fm0,-Gm0),axis=0)
+    RVec=np.linalg.solve(MatBC,VecBC)
+    #绘制衍射效率曲线
+    etaR,etaT=Plot_intensity(Constant,RVec,real_Ray1_idx,real_Ray2_idx,B1,B2,m1,b0,nDim,False)
+    etaR=etaR.flatten()
+    etaT=etaT.flatten()
+    return etaR,etaT
+
+def Roughness(a_func,Ra=0.0,seed=None):
+    '''
+    为原始光栅轮廓函数附加粗糙度
+    参数:
+    a_func:原始轮廓函数
+    Ra:粗糙度幅度,使用μm作为单位
+    seed:随机种子
+    '''
+    if seed is not None:
+        np.random.seed(seed)
+    required_std=Ra/np.sqrt(2/np.pi)
+    if Ra>0:
+        noise=np.random.normal(0,required_std,size=a_func.shape)
+        return a_func+noise
+    else:
+        return a_func
+
+def SortEigenvalueChand(V1,rho1,V2,rho2,tol,Constant):
+    nDim=Constant['n_Tr']
+    #=============处理D1区域================================
+    n_prop1=len(Constant['real_Ray1_idx'])#传播级的数量
+
+    cand1_idx=np.where(np.real(rho1)>0)[0]
+    cand1=rho1[cand1_idx]
+
+    idx_sort1=np.argsort(np.abs(np.imag(cand1)))
+    selected_idx1=cand1_idx[idx_sort1[:nDim]]
+
+    prop_idx1=selected_idx1[:n_prop1]
+    evan_idx1=selected_idx1[n_prop1:]
+
+    real_eig1p=rho1[prop_idx1]
+    imag_eig1p=rho1[evan_idx1]
+
+    real_vec1p=V1[:nDim,prop_idx1]
+    imag_vec1p=V1[:nDim,evan_idx1]
+    ###传播模,按实部降序
+    idx_prop1=np.argsort(-np.real(real_eig1p))
+    real_eig1p=real_eig1p[idx_prop1]
+    real_vec1p=real_vec1p[:,idx_prop1]
+    ###倏逝模,按虚部升序
+    idx_evan1=np.argsort(np.abs(np.imag(imag_eig1p)))
+    imag_eig1p=imag_eig1p[idx_evan1]
+    imag_vec1p=imag_vec1p[:,idx_evan1]
+    #=========处理D2区域================
+    n_prop2=len(Constant['real_Ray2_idx'])
+
+    cand2_idx=np.where(np.real(rho2)<0)[0]
+    cand2=rho2[cand2_idx]
+
+    idx_sort2=np.argsort(np.abs(np.imag(cand2)))
+    selected_idx2=cand2_idx[idx_sort2[:nDim]]
+
+    prop_idx2=selected_idx2[:n_prop2]
+    evan_idx2=selected_idx2[n_prop2:]
+
+    real_eig2n=rho2[prop_idx2]
+    imag_eig2n=rho2[evan_idx2]
+
+    real_vec2n=V2[:nDim,prop_idx2]
+    imag_vec2n=V2[:nDim,evan_idx2]
+
+    ###传播模,按实部绝对值升序
+    idx_prop2=np.argsort(np.abs(np.real(real_eig2n)))
+    real_eig2n=real_eig2n[idx_prop2]
+    real_vec2n=real_vec2n[:,idx_prop2]
+    ###倏逝模,按虚部升序
+    idx_evan2=np.argsort(np.abs(np.imag(imag_eig2n)))
+    imag_eig2n=imag_eig2n[idx_evan2]
+    imag_vec2n=imag_vec2n[:,idx_evan2]
+    return real_eig1p,real_eig2n,imag_eig1p,imag_eig2n,imag_vec1p,imag_vec2n
+
+##################设定仿真常数区域##########################################
+Constant={}
+n1=1
+n2=1.4482+7.5367j
+# n2=1.5
+Constant['n1']=n1
+Constant['n2']=n2
+Constant['thetai']=np.radians(0)
+Constant['n_Tr']=2*20+1
+Constant['wavelength']=632.8*1e-9
+Constant['k0']=2*np.pi/Constant['wavelength']
+#set Accuracy
+Constant['cut']=0#是否对变换后的傅里叶级数进行去除小数处理
+Constant['accuracy']=1e-9
+Constant['kVecImagMin']=1e-10
+R_effi=[]
+##########################################################################
+
+##########以下为光栅常数设定##############
+# grating=Triangular(4*1e-6,36,1)
+depth=Constant['wavelength']
+grating=Sinusoidal(2*Constant['wavelength'],1,depth)
+Constant['period']=grating.T
+Constant['depth']=depth
+a=grating.profile()
+x=np.linspace(0,Constant['period'],2**10,endpoint=False)
+Constant['a']=Constant['k0']*a(x)#光栅表面轮廓函数
+dx=Constant['period']*Constant['k0']/len(x)
+a_diff=np.gradient(Constant['a'],dx)
+# plt.plot(x,Constant['a'])
+# plt.plot(x,a_diff)
+# plt.show()
+#############################################
+
+##########任意偏振态,为TE、TM偏振态的组合###############################
+alpha=90
+alpha=np.radians(alpha)
+TM=np.cos(alpha)#TM模式的分量
+TE=np.sin(alpha)#TE模式的分量
+if TM<1e-10:
+    TM=0
+if TE<1e-10:
+    TE=0
+if TM!=0:
+    Polarization='TM'
+    etaR_TM,etaT_TM=Compute(n1,n2,Polarization,Constant)
+if TE!=0:
+    Polarization='TE'
+    etaR_TE,etaT_TE=Compute(n1,n2,Polarization,Constant)
+if TM==0:
+    etaR_TM=np.zeros_like(etaR_TE)
+if TE==0:
+    etaR_TE=np.zeros_like(etaR_TM)
+polar=TM**2*etaR_TM+TE**2*etaR_TE
+Constant['R_effi']=polar
+######################################################################
+real_Ray1_idx=Constant['real_Ray1_idx']
+Plot_Effi(Constant,[],False)
---
+++ C_Method_Sin3.py
@@ -0,0 +1,330 @@
+import numpy as np
+import sys
+sys.path.append('E:/Project/python')
+from S_matrix.Grating import Sinusoidal
+from scipy import integrate
+from scipy.linalg import toeplitz
+import scipy.linalg as la
+
+def SetConstant(n1,n2,polar,period,n_Tr,lam,thetai):
+    '''
+    d:光栅周期,h:槽深,profile:光栅槽型,n1、n2:入射、出射区域折射率,
+    thetai:入射角,lam:波长,polar:极化状态,numTr:截断阶数,tol:误差等级
+    '''
+    eps0=8.8541878171*1e-12
+    mu0=12.5663706141*1e-7
+    mu1=1
+    mu2=1
+    eps1=n1**2/mu1
+    eps2=n2**2/mu2
+    if polar=='TE':
+        mu0,eps0=-eps0,-mu0
+        mu1,eps1=eps1,-mu1
+        mu2,eps2=eps2,-mu2
+    Z0=np.sqrt(mu0/eps0)
+    Constant={}
+    Constant['n1']=n1
+    Constant['n2']=n2
+    Constant['eps0']=eps0
+    Constant['eps1']=eps1
+    Constant['eps2']=eps2
+    Constant['mu0']=mu0
+    Constant['mu1']=mu1
+    Constant['mu2']=mu2
+    Constant['Z0']=Z0
+    Constant['k0']=2*np.pi/lam
+    Constant['K']=2*np.pi/period
+    Constant['n_Tr']=n_Tr
+    m1=int(-((n_Tr-1)/2))
+    m2=int(-m1)
+    m_set=np.linspace(m1,m2,m2-m1+1,dtype=int)
+    n_set=np.linspace(1-n_Tr,n_Tr-1,2*n_Tr-1,dtype=int)
+    Constant['m_set']=m_set
+    Constant['n_set']=n_set
+    alpha_m=n1*Constant['k0']*np.sin(thetai)+Constant['K']*m_set
+    beta1_m=np.sqrt(np.diag(np.eye(n_Tr)*(n1*Constant['k0'])**2-np.diag(alpha_m**2)),dtype=complex)
+    beta2_m=np.sqrt(np.diag(np.eye(n_Tr)*(n2*Constant['k0'])**2-np.diag(alpha_m**2)),dtype=complex)
+    Constant['alpha_m']=alpha_m
+    Constant['beta1_m']=beta1_m
+    Constant['beta2_m']=beta2_m
+
+    idx=np.where(np.abs(np.imag(beta1_m))<1e-10)
+    n1_set=m_set[idx]
+    n1_set_ind=idx
+    Constant['n1_set']=n1_set
+    Constant['n1_set_ind']=n1_set_ind[0]
+
+    idx=np.where(np.abs(np.imag(beta2_m))<1e-10)
+    n2_set=m_set[idx]
+    n2_set_ind=idx
+    Constant['n2_set']=n2_set
+    Constant['n2_set_ind']=n2_set_ind[0]
+    return Constant
+
+def Eigen(a_mat,alpha_m,beta1_m,beta2_m,Constant):
+    IB1=np.linalg.solve(np.diag(beta1_m**2),np.eye(Constant['n_Tr']))
+    IB2=np.linalg.solve(np.diag(beta2_m**2),np.eye(Constant['n_Tr']))
+    AUX_mat=np.eye(Constant['n_Tr'])+a_mat@a_mat
+    Matrix1=np.block([[-IB1@(np.diag(alpha_m)@a_mat+a_mat@np.diag(alpha_m)),IB1@AUX_mat],[np.eye(Constant['n_Tr']),np.zeros((Constant['n_Tr'],Constant['n_Tr']))]])
+    Matrix2=np.block([[-IB2@(np.diag(alpha_m)@a_mat+a_mat@np.diag(alpha_m)),IB2@AUX_mat],[np.eye(Constant['n_Tr']),np.zeros((Constant['n_Tr'],Constant['n_Tr']))]])
+    eig1,vec1=np.linalg.eig(Matrix1)
+    eig2,vec2=np.linalg.eig(Matrix2)
+    eig1=1/eig1
+    eig2=1/eig2
+    return eig1,vec1,eig2,vec2
+
+def SortEigenvalue(eig,vect,accuracyImag):
+    idxRealAndPositive=((np.abs(np.imag(eig))<accuracyImag)&(np.real(eig)>0))
+    idxRealAndNegative=((np.abs(np.imag(eig))<accuracyImag)&(np.real(eig)<=0))
+    idxImagAndPositive=((np.abs(np.imag(eig))>=accuracyImag)&(np.imag(eig)>0))
+    idxImagAndNegative=((np.abs(np.imag(eig))>=accuracyImag)&(np.imag(eig)<=0))
+
+    eig_real_p=eig[idxRealAndPositive]
+    vect_real_p=vect[:,idxRealAndPositive]
+    eig_real_n=eig[idxRealAndNegative]
+    vect_real_n=vect[:,idxRealAndNegative]
+    eig_imag_p=eig[idxImagAndPositive]
+    vect_imag_p=vect[:,idxImagAndPositive]
+    eig_imag_n=eig[idxImagAndNegative]
+    vect_imag_n=vect[:,idxImagAndNegative]
+
+    if len(eig_real_p)!=0:
+        ind=np.argsort(eig_real_p.real)[::-1]
+        eig_real_p=eig_real_p[ind]
+        vect_real_p=vect_real_p[:,ind]
+    if len(eig_imag_p)!=0:
+        ind=np.argsort(eig_imag_p.imag)
+        eig_imag_p=eig_imag_p[ind]
+        vect_imag_p=vect_imag_p[:,ind]
+    if len(eig_real_n)!=0:
+        ind=np.argsort(eig_real_n.real)
+        eig_real_n=eig_real_n[ind]
+        vect_real_n=vect_real_n[:,ind]
+    if len(eig_imag_n)!=0:
+        ind=np.argsort(eig_imag_n.imag)[::-1]
+        eig_imag_n=eig_imag_n[ind]
+        vect_imag_n=vect_imag_n[:,ind]
+    
+    eig_p=np.block([eig_real_p,eig_imag_p])
+    vect_p=np.block([vect_real_p,vect_imag_p])
+    eig_n=np.block([eig_real_n,eig_imag_n])
+    vect_n=np.block([vect_real_n,vect_imag_n])
+    return eig_p,vect_p,eig_n,vect_n
+
+def L_eval(gamma,m,Constant):
+    def integrand(x):
+        if isinstance(Constant['a(x)'],str):
+            a_x_val=eval(Constant['a(x)'],{"np":np,"x":x})
+        else:
+            a_x_val=Constant['a(x)'](x)
+        exponent=1j*gamma*a_x_val-1j*m*2*np.pi*x/Constant['period']
+        return (1/Constant['period'])*np.exp(exponent)
+    L,error=integrate.quad(integrand,0,Constant['period'],complex_func=True)
+    return L
+
+def GenerateFField(Constant):
+    m_set=Constant['m_set']
+    n1_set=Constant['n1_set']
+    n1_set_ind=Constant['n1_set_ind']
+    n2_set=Constant['n2_set']
+    n2_set_ind=Constant['n2_set_ind']
+    beta1_m=Constant['beta1_m']
+    beta2_m=Constant['beta2_m']
+    idx_m=0
+    idx_n=0
+    #=====Fmn=====
+    if len(Constant['n1_set'])!=0:
+        Fmn=np.zeros((len(m_set),len(n1_set)),dtype=complex)
+        for m in range(m_set[0],m_set[-1]+1,1):
+            for n in range(n1_set[0],n1_set[-1],1):
+                Fmn[idx_m,idx_n]=L_eval(beta1_m[n1_set_ind[idx_n]],m-n,Constant)
+                idx_n+=1
+            idx_n=0
+            idx_m+=1
+    else:
+        Fmn=np.zeros(len(m_set),len(n1_set))
+    #======Fmk========
+    idx_m=0
+    idx_k=0
+    if len(Constant['n2_set'])!=0:
+        Fmk=np.zeros(len(m_set),len(n2_set))
+        for m in range(m_set[0],m_set[-1]+1,1):
+            for k in range(n2_set[0],n2_set[-1]+1,1):
+                Fmk[idx_m,idx_k]=L_eval(-beta2_m[n2_set_ind[idx_k]],m-k,Constant)
+                idx_k+=1
+            idx_k=0
+            idx_m+=1
+    else:
+        Fmk=np.zeros((len(m_set),len(n2_set)))
+    #=====F_in_0===========
+    idx=0
+    Fm0=np.zeros((len(m_set),1),dtype=complex)
+    for m in range(m_set[0],m_set[-1]+1,1):
+        Fm0[idx,0]=L_eval(-beta1_m[int((Constant['n_Tr']+1)/2)],m,Constant)
+        idx+=1
+    #===========Fmq=================
+    Fmq=vect1_p[:Constant['n_Tr'],len(n1_set):]
+    #===========Fmr=================
+    Fmr=vect2_n[:Constant['n_Tr'],len(n2_set):]
+    Constant['Fmn']=Fmn
+    Constant['Fmk']=Fmk
+    Constant['Fm0']=Fm0
+    Constant['Fmq']=Fmq
+    Constant['Fmr']=Fmr
+    return Fmn,Fmk,Fm0,Fmq,Fmr
+
+def GenerateGField(a_mat,Constant,eig1_p,eig2_n):
+    a_ms=np.eye(Constant['n_Tr'])+a_mat@a_mat
+    n1_set=Constant['n1_set']
+    n1_set_ind=Constant['n1_set_ind']
+    n2_set=Constant['n2_set']
+    n2_set_ind=Constant['n2_set_ind']
+    m_set=Constant['m_set']
+    alpha_m=Constant['alpha_m']
+    beta1_m=Constant['beta1_m']
+    beta2_m=Constant['beta2_m']
+    Fmn=Constant['Fmn']
+    Fmk=Constant['Fmk']
+    Fm0=Constant['Fm0']
+    Fmq=Constant['Fmq']
+    Fmr=Constant['Fmr']
+    #===========Gmn=========================
+    if len(Constant['n1_set_ind'])!=0:
+        Gmn=np.zeros((len(m_set),len(n1_set)),dtype=complex)
+        for m in range(len(m_set)):
+            for n in range(len(n1_set)):
+                Gmn_s=np.zeros(len(m_set),dtype=complex)
+                for s in range(len(m_set)):
+                    Gmn_s[s]=(a_mat[m,s]*alpha_m[s]-a_ms[m,s]*beta1_m[n1_set_ind[n]])*Fmn[s,n]
+                Gmn[m,n]=Constant['Z0']/Constant['k0']/Constant['eps1']*sum(Gmn_s)
+    else:
+        Gmn=np.zeros((len(m_set),len(n1_set)))
+    #==========Gmk==========================
+    if len(Constant['n2_set_ind'])!=0:
+        Gmk=np.zeros((len(m_set),len(n2_set)),dtype=complex)
+        for m in range(len(m_set)):
+            for k in range(len(n2_set_ind)):
+                Gmk_s=np.zeros((len(m_set),1))
+                for s in range(len(m_set)):
+                    Gmk_s[s,0]=(a_mat[m,s]*alpha_m[s]+a_ms[m,s]*beta2_m[n2_set_ind[k]])*Fmk[s,k]
+                Gmk[m,k]=Constant['Z0']/Constant['k0']/Constant['eps2']*sum(Gmk_s)
+    else:
+        Gmk=np.zeros((len(m_set),len(n2_set)),dtype=complex)
+    #=================Gm0=========================
+    Gm0=np.zeros((len(m_set),1),dtype=complex)
+    for m in range(len(m_set)):
+        Gm0_s=np.zeros((len(m_set),1),dtype=complex)
+        for s in range(len(m_set)):
+            Gm0_s[s,0]=(a_mat[m,s]*alpha_m[s]+a_ms[m,s]*beta1_m[int((Constant['n_Tr']-1)/2)])*Fm0[s][0]
+        Gm0[m,0]=Constant['Z0']/Constant['k0']/Constant['eps1']*sum(Gm0_s)
+    #=================Gmq==========================
+    if len(n1_set)<Constant['n_Tr']:
+        Gmq=np.zeros((len(m_set),Constant['n_Tr']-len(n1_set)),dtype=complex)
+        for m in range(len(m_set)):
+            for q in range(Constant['n_Tr']-len(n1_set)):
+                Gmq_s=np.zeros((len(m_set),1))
+                for s in range(len(m_set)):
+                    Gmq_s[s,0]=(a_mat[m,s]*alpha_m[s]-a_ms[m,s]*eig1_p[len(n1_set)+q])*Fmq[s,q]
+                Gmq[m,q]=Constant['Z0']/Constant['k0']/Constant['eps1']*sum(Gmq_s)
+    else:
+        Gmq=np.zeros((len(m_set),Constant['n_Tr']-len(n1_set)),dtype=complex)
+    #==================Gmr==========================
+    if len(n2_set)<Constant['n_Tr']:
+        Gmr=np.zeros((len(m_set),Constant['n_Tr']-len(n2_set)),dtype=complex)
+        for m in range(len(m_set)):
+            for r in range(Constant['n_Tr']-len(n2_set)):
+                Gmr_s=np.zeros((len(m_set),1))
+                for s in range(len(m_set)):
+                    Gmr_s[s]=(a_mat[m,s]*alpha_m[s]-a_ms[m,s]*eig2_n[len(n2_set)+r])*Fmr[s,r]
+                Gmr_s=Gmr_s.flatten()
+                Gmr[m,r]=Constant['Z0']/Constant['k0']/Constant['eps2']*sum(Gmr_s)
+    else:
+        Gmr=np.zeros((len(m_set),Constant['n_Tr']-len(n2_set)),dtype=complex)
+    return Gmn,Gmk,Gm0,Gmq,Gmr
+
+def CutSmallElement(a,accuracy):
+    idx=(np.abs(a)<accuracy)
+    a[idx]=0
+    return a
+
+def ComputeAdiff(Constant):
+    n_set=Constant['n_set']
+    a_diff=np.zeros(len(n_set))
+    i=0
+    for n in range(n_set[0],n_set[-1]+1):
+        F=lambda x:(1/Constant['period'])*np.exp(-1j*n*Constant['K']*x)*Constant['a_diff(x)'](x)
+        a_diff[i],error=integrate.quad(F,0,Constant['period'])
+        i+=1
+    a_diff=CutSmallElement(a_diff,1e-10)
+    return a_diff
+
+####=============指定仿真材料等参数================
+n1=1
+n2=1.4482+7.5367j
+pol='TE'
+period=4*1e-6
+n_Tr=2*12+1
+lam=632.8*1e-9
+thetai=np.radians(0)
+depth=2*1e-6#光栅深度
+ImagMin=1e-9
+cut=1#需要修剪数据
+accuracy=1e-10
+#==================================================
+Constant=SetConstant(n1,n2,pol,period,n_Tr,lam,thetai)
+Constant['ImagMin']=ImagMin
+grating=Sinusoidal(period,1,depth)
+Constant['period']=period
+a=grating.profile()
+Constant['a(x)']=a
+Constant['a_diff(x)']=grating.a_diff()
+a_diff_vec=ComputeAdiff(Constant)
+a_col=a_diff_vec[Constant['n_Tr']-1:]
+a_row=a_diff_vec[Constant['n_Tr']-1::-1]
+# x=np.linspace(0,Constant['period'],2**10,endpoint=False)
+# Constant['a']=Constant['k0']*a(x)#光栅表面轮廓函数
+# dx=Constant['period']*Constant['k0']/len(x)
+# a_diff=np.gradient(Constant['a'],dx)
+# a_diff_vec=F_series_gen(a_diff,n_Tr)
+a_mat=toeplitz(a_col,a_row)
+eig1,vect1,eig2,vect2=Eigen(a_mat,Constant['alpha_m'],Constant['beta1_m'],Constant['beta2_m'],Constant)
+eig1_p,vect1_p,_,_=SortEigenvalue(eig1,vect1,Constant['ImagMin'])
+_,_,eig2_n,vect2_n=SortEigenvalue(eig2,vect2,Constant['ImagMin'])
+Constant['vect1_p']=vect1_p
+Constant['vect2_n']=vect2_n
+Fmn,Fmk,Fm0,Fmq,Fmr=GenerateFField(Constant)
+if cut==1:
+    Fmn=CutSmallElement(Fmn,accuracy)
+    Fmk=CutSmallElement(Fmk,accuracy)
+    Fm0=CutSmallElement(Fm0,accuracy)
+    Fmq=CutSmallElement(Fmq,accuracy)
+    Fmr=CutSmallElement(Fmr,accuracy)
+Gmn,Gmk,Gm0,Gmq,Gmr=GenerateGField(a_mat,Constant,eig1_p,eig2_n)
+GF_matrix=np.block([[Fmn,Fmq,-Fmk,-Fmr],[Gmn,Gmq,-Gmk,-Gmr]])
+GF_col=-np.block([[Fm0],[Gm0]])
+##==============固定碰到大条件数矩阵=====================
+U,s,Vt=la.svd(GF_matrix,full_matrices=False)
+threshold=np.max(s)*np.max(GF_matrix.shape)*np.finfo(float).eps
+s_inv=np.zeros_like(s)
+mask=(s>threshold)
+s_inv[mask]=1.0/s[mask]
+s_inv=s_inv.reshape(-1,1)
+Amplitude=Vt.T@(s_inv*(U.T@GF_col))
+Amplitude=Amplitude.flatten()
+# Amplitude=np.linalg.solve(GF_matrix,GF_col)
+#==============Reflection efficiency=================
+R=np.zeros(len(Constant['n1_set']))
+beta1_m=Constant['beta1_m']
+n1_set_ind=Constant['n1_set_ind']
+for i in range(len(Constant['n1_set'])):
+    R[i]=beta1_m[n1_set_ind[i]]/beta1_m[int((Constant['n_Tr']-1)/2)]*(abs(Amplitude[i])**2)
+#=============Transmission efficiency==============
+T=np.zeros(len(Constant['n1_set']))
+beta2_m=Constant['beta2_m']
+n2_set=Constant['n2_set']
+n2_set_ind=Constant['n2_set_ind']
+if len(n2_set)!=0:
+    for i in range(len(n2_set)):
+        T[i]=(Constant['eps1']*beta2_m[n2_set_ind[i]]/(Constant['eps2']*beta1_m[(Constant['n_Tr']+1)/2]))*abs(Amplitude[Constant['n_Tr']+i])**2
+print(R)
+print("sum R:{}".format(sum(R)))
