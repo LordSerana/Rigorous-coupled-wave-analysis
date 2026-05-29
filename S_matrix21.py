9475e23a7c9daa3093eb259aa020fec5c11320cd
Serana <568324604@qq.com>
2026-05-29 10:50:59 +0800

    2026.5.29

--- S_matrix21.py
+++ S_matrix21.py
@@ -9,6 +9,7 @@ from S_matrix.Star import Star
 from S_matrix.Build_scatter_side import build_scatter_side
 from S_matrix.CalcEffi import calcEffi
 from S_matrix.Plot_Effi import Plot_Effi
+from openpyxl import load_workbook
 
 def Set_Polarization(thetai,phi,n1,n2,wavelength,pTE,pTM,m,Nx,accuracy,grating):
     '''
@@ -123,7 +124,32 @@ layers=[
 # grating=Triangular(4*1e-6,30,1)
 grating=Blazed(4*1e-6,30,1,1)
 #====================================================
-Constant=Set_Polarization(0,0,1,1.4482+7.5367j,632.8*1e-9,1,0,20,2**8,1e-9,grating)
-layers=Slice(layers,grating,10)
-Constant=Compute(Constant,layers)
-Plot_Effi(Constant,[],[])
+Constant=Set_Polarization(0,0,1,1.4482+7.5367j,632.8*1e-9,1,0,50,2**8,1e-9,grating)
+# n=15
+# layers=Slice(layers,grating,n)
+# Constant=Compute(Constant,layers)
+# Plot_Effi(Constant,[],[])
+file_path='C:/Users/123/Desktop/闪耀光栅30仿真数据.xlsx'
+wb=load_workbook(file_path)
+ws=wb.active
+start_col=2
+start_row=17#行
+counter=0
+save_interval=5
+for n in range(2,51):
+    layers=[
+        Layer(n=1,t=1*1e-6),
+        Layer(n=1.4482+7.5367j,t=2*1e-6,fill_factor=1,offset=0.5),
+        Layer(n=1.4482+7.5367j,t=4*1e-6)
+        ]
+    layers=Slice(layers,grating,n)
+    Constant=Compute(Constant,layers)
+    R5=Constant['R_effi'][1]
+    ws.cell(row=start_row,column=start_col,value=R5)
+    counter+=1
+    start_row+=1
+    if counter%save_interval==0:
+        wb.save(file_path)
+        print(f"已保存{counter}个数据")
+# ws.cell(row=start_row,column=start_col,value="切片数:{},N={}".format(n,101))
+wb.save(file_path)
